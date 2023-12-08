468a5acc38e5601a4900746ae1be107b3669aff3
katfogy <katfogy@gmail.com>
2023-12-08 12:05:06 +0100

    Seed data added

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,35 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+# 30.times do
+#     User.create!(
+#         email: Faker::Internet.email,
+#         password: "secret123",
+#         name: Faker::Name.name,
+#         address: Faker::Address.full_address
+#     )
+# end
+
+# 5.times do(
+#     Company.create!(
+#         name:Faker::Company.name,
+#     )
+# )
+# end
+
+# Category.create([{name:"House"}, {name:"Car"}, {name:"Motor", }, {name:"Gold"}])
+
+20.times do
+    Product.create!(
+        name: Faker::Commerce.product_name,
+        price: Faker::Commerce.price,
+        img: Faker::LoremFlickr.image(size:"300x300", search_terms:['product']),
+        description: Faker::Lorem.paragraph,
+        company_id: Company.all.sample.id,
+        category_id: Category.all.sample.id
+    )
+end
+
+70.times do
+    Order.create!(
+        product_id: Product.all.sample.id,
+        user_id: User.all.sample.id
+    )
+end
